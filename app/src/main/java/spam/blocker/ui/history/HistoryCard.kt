913d9b2b965f45387bedd8f8fb1b096d6a1ac9c4
aj3423 <aj3423@gmail.com>
2024-10-30 14:16:34 +0800

    fix history time height

--- app/src/main/java/spam/blocker/ui/history/HistoryCard.kt
+++ app/src/main/java/spam/blocker/ui/history/HistoryCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -128,7 +129,7 @@ fun HistoryCard(
             // 3. Time / Unread Indicator
             Box(
                 modifier = M
-                    .height(ItemHeight.dp)
+                    .heightIn(min = ItemHeight.dp)
                     .align(Alignment.Top)
             ) {
                 // time
