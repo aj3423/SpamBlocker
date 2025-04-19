2e580eec454f83aea39f040944f26e459f86fe0d
aj3423 <aj3423@gmail.com>
2025-04-19 23:56:04 +0800

    v4.10 ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "spam.blocker"
         minSdk = 29
         targetSdk = 35
-        versionCode = 409
-        versionName = "4.9"
+        versionCode = 410
+        versionName = "4.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
