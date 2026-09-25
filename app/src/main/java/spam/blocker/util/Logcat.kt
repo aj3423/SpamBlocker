d77a069f72e7c12069d65d86d9f2f175bf3b8cee
aj3423 <aj3423@gmail.com>
2026-09-26 01:28:54 +0800

    .

--- app/src/main/java/spam/blocker/util/Logcat.kt
+++ app/src/main/java/spam/blocker/util/Logcat.kt
@@ -2,18 +2,28 @@ package spam.blocker.util
 
 import android.util.Log
 import spam.blocker.G
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 const val TAG = "SpamBlocker"
 
+private fun now() : String {
+    val millis = System.currentTimeMillis()
+    val sdf = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+    val formattedTime = sdf.format(Date(millis))
+    return "$millis $formattedTime"
+}
+
 fun logd(str: String) {
-    G.debugText.value += str + "\n"
+    G.debugText.value += now() + " " + str + "\n"
 }
 fun logi(str: String) {
-    G.debugText.value += str + "\n"
+    G.debugText.value += now() + " " + str + "\n"
 }
 fun logw(str: String) {
-    G.debugText.value += str + "\n"
+    G.debugText.value += now() + " " + str + "\n"
 }
 fun loge(str: String) {
-    G.debugText.value += str + "\n"
+    G.debugText.value += now() + " " + str + "\n"
 }
